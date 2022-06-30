712fe21619d74254363bdc7599b10e50eb8cb8b4
Suad Suljovic <suad.suljovic92@gmail.com>
2022-06-30 18:53:43 +0200

    prvi cas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Card from "./components/functional/Card";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <Card
+        title="Aisa Nicevic"
+        job="Najbolji Pyton Coder"
+        url="https://www.w3schools.com/howto/img_avatar.png"
+      />
+      <Card
+        title="Suad Suljovic"
+        job="Developer"
+        url="https://www.w3schools.com/howto/img_avatar.png"
+      />
+      <Card
+        title="Irfan Ugljanin"
+        job="Ne prestaje da prica"
+        url="https://www.w3schools.com/howto/img_avatar.png"
+      />
+    </div>
   </React.StrictMode>
 );
 
