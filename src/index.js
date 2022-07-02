4d1c7ebb750e0aed18538c56a1c5676362f2d0b9
Suad Suljovic <suad.suljovic92@gmail.com>
2022-07-02 18:56:27 +0200

    class 2

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Card from "./components/functional/Card";
 
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      <Card
-        title="Aisa Nicevic"
-        job="Najbolji Pyton Coder"
-        url="https://www.w3schools.com/howto/img_avatar.png"
-      />
-      <Card
-        title="Suad Suljovic"
-        job="Developer"
-        url="https://www.w3schools.com/howto/img_avatar.png"
-      />
-      <Card
-        title="Irfan Ugljanin"
-        job="Ne prestaje da prica"
-        url="https://www.w3schools.com/howto/img_avatar.png"
-      />
-    </div>
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
